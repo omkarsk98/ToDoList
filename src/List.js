ebf2e18a4262d105d6e91cb7fe8d7c32bcb3a1a9
programmer <omkarsk98@gmail.com>
2022-10-25 15:48:58 -0700

    Formatted dependencies and fixed bugs

---
+++ src/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class List extends React.Component {
+	constructor(props) {
+		super(props);
+		this.createTask = this.createTask.bind(this);
+	}
+	delete(key) {
+		this.props.delete(key);
+	}
+
+	createTask(b1) {
+		return <li key={b1.key}> {b1.task} <button onClick={() => this.delete(b1.key)}>X</button> </li>
+	}
+
+	render() {
+		return (
+			<div>
+				<ul>
+					{this.props.items.map(this.createTask)}
+				</ul>
+				<ul>
+					End of List
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default List;
